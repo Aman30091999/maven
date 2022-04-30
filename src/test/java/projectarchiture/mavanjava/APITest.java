9a876e1c752becfcf3c4e51de07c204fb112a44d
Akum96392@gmail.com <Akum96392@gmail.com>
2022-04-30 17:50:48 +0530

    changes

--- src/test/java/projectarchiture/mavanjava/APITest.java
+++ src/test/java/projectarchiture/mavanjava/APITest.java
@@ -7,10 +7,13 @@ public class APITest {
 	@Test(groups={"Regression"})
 	public void apihit() {
 		System.out.println("API Hit");
+		System.out.println("new code");
 	}
 
 	@Test
 	public void apiStart() {
 		System.out.println("API Start");
+		System.out.println("new code");
+
 	}
 }
